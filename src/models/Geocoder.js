6bff7b54f06bba454ffc8b5cc89eae2b3bf4c54e
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-09-26 15:16:48 -0400

    set esri search widget's includeDefaultSources to false

--- src/models/Geocoder.js
+++ src/models/Geocoder.js
@@ -2,7 +2,7 @@ function Geocoder () {}
 
 Geocoder.esriSearchSource = {
   locator: {
-    // url: 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer'
+    // url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer'
     // url: 'https://maps.hillsboroughcounty.org/arcgis/rest/services/Geocoding/DBO_composite_address_locator/GeocodeServer'
     // url: 'https://maps.hillsboroughcounty.org/arcgis/rest/services/Geocoding/Composite_Address_Locator_WebApps/GeocodeServer'
     url: 'https://maps.hillsboroughcounty.org/arcgis/rest/services/Geocoding/Composite_Address_Locator_Overall/GeocodeServer'
